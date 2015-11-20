a4f69546a307fccb25eb381b7c5f7d3a90f30998
ideyu <gy4ok4ok@gmail.com>
2015-11-20 12:59:47 +0200

    add show.erb

--- my_blog/app/controllers/articles_controller.rb
+++ my_blog/app/controllers/articles_controller.rb
@@ -4,4 +4,15 @@ class ArticlesController < ApplicationController
 
   def new
   end
+
+  def create
+    @params = Article.new(params.require(:article).permit(:author, :text))
+    @params.save
+    redirect_to @params
+    #render plain: params[:article].inspect
+  end
+
+  def show
+    @article = Article.find(1)
+  end
 end
